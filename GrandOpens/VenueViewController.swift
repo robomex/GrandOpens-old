d36d9974e4f93caf121003334933b551de251fb0
robomex <atmorales@gmail.com>
2015-07-21 18:23:52 -0500

    VenueViewController

---
+++ GrandOpens/VenueViewController.swift
@@ -0,0 +1,39 @@
+//
+//  VenueViewController.swift
+//  GrandOpens
+//
+//  Created by Tony Morales on 7/21/15.
+//  Copyright (c) 2015 Tony Morales. All rights reserved.
+//
+
+import Foundation
+import Parse
+
+class VenueViewController: JSQMessagesViewController {
+    
+    var messages: [JSQMessage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func senderDisplayName() -> String! {
+        return currentUser()!.id
+    }
+    
+    func senderId() -> String! {
+        return currentUser()!.id
+    }
+    
+    override func collectionView(collectionView: JSQMessagesCollectionView!, messageDataForItemAtIndexPath indexPath: NSIndexPath!) -> JSQMessageData! {
+        
+        var data = self.messages[indexPath.row]
+        return data
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+}
